9d2e41d06c916925494bc4b29f8f28afe53b580f
Anastasia Novitskaya <nastasya11011995@gmail.com>
2023-03-16 21:20:21 +1200

    task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите цифру дня недели");
+ int day = Convert.ToInt32(Console.ReadLine());
+ if (day <1 || day > 7)
+ {
+    Console.WriteLine("Несуществующий день недели");
+ }
+ else
+ {
+    if (day >= 1 & day <= 5)
+    {
+        Console.WriteLine("Рабочий день");
+    }
+    else
+    {
+        Console.WriteLine("выходной день");
+    }
+ }
+
