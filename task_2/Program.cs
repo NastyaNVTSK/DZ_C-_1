0e9b53c526abe66023967f1142adad86df1994bb
Anastasia Novitskaya <nastasya11011995@gmail.com>
2023-03-16 21:16:20 +1200

    task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 100)
+{
+    Console.WriteLine((num / 100) % 10);
+}
+else
+{
+    Console.WriteLine("Нет третьей цифры");
+}
+
